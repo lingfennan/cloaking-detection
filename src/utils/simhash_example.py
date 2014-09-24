083527d12380cbab91471f1419da19fd51db6236
Ruian Duan <duanruian@gmail.com>
2014-09-24 16:41:28 -0400

    Add simhash_example.py

---
+++ src/utils/simhash_example.py
@@ -0,0 +1,25 @@
+from simhash import Simhash, SimhashIndex
+print '%x' % Simhash('How are you? I am fine. Thanks.').value
+print '%x' % Simhash('How are u? I am fine.     Thanks.').value
+print '%x' % Simhash('How r you?I    am fine. Thanks.').value
+
+print Simhash('aa').distance(Simhash('bb'))
+print Simhash('aa').distance(Simhash('aa'))
+
+data = {
+	1: u'How are you? I Am fine. blar blar blar blar blar Thanks.',
+	2: u'How are you i am fine. blar blar blar blar blar than',
+	3: u'This is simhash test.',
+}
+objs = [(str(k), Simhash(v)) for k, v in data.items()]
+index = SimhashIndex(objs)
+
+print index.bucket_size()
+
+s1 = Simhash(u'How are you i am fine. blar blar blar blar blar thank')
+print index.get_near_dups(s1)
+
+index.add('4', s1)
+print index.get_near_dups(s1)
+
+
