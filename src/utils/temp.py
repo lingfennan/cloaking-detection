67c171de6347dffa380fc9e22a93561d2881cd5a
Ruian Duan <duanruian@gmail.com>
2014-11-17 11:25:31 -0500

    Test file temp.py

---
+++ src/utils/temp.py
@@ -0,0 +1,17 @@
+from learning_detection_util import load_observed_sites, write_proto_to_file, read_proto_from_file, valid_instance
+import proto.cloaking_detection_pb2 as CD
+
+crawl_log = CD.CrawlLog()
+in_filename = 'data/US_web_search_list.Chrome.20141110-185317.selenium.crawl/crawl_log'
+read_proto_from_file(crawl_log, in_filename)
+suc_counter = 0
+fail_counter = 0
+for result in crawl_log.result:
+	if result.success:
+		suc_counter += 1
+	else:
+		fail_counter += 1
+
+print suc_counter
+print fail_counter
+# print crawl_log
