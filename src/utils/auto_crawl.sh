883adbaf07d375b62e37eb38bf1672610717b27b
Ruian Duan <duanruian@gmail.com>
2014-11-21 12:55:07 -0500

    Moved the data directory to root. Fixed the script.

--- src/utils/auto_crawl.sh
+++ src/utils/auto_crawl.sh
@@ -1,12 +1,13 @@
 # start from today 10:00 o'clock
-hour="10:00"
-gap="2 days"
+googlehour="10:00"
+nongooglehour="12:36"
+gap="1 days"
 nextdate=$(date +%Y-%m-%d)
 while true;
 do
 	# sleep until specific time
-	difference=$(($(date -d "$nextdate $hour" +%s) - $(date +%s)))
-	echo "Next run is on $nextdate $hour"
+	difference=$(($(date -d "$nextdate $nongooglehour" +%s) - $(date +%s)))
+	echo "Next run is on $nextdate $nongooglehour"
 	if [ $difference -lt 0 ]
 	then
 		echo "Sleeping $((86400 + difference)) seconds"
@@ -16,7 +17,27 @@ do
 		sleep $difference
 	fi
 	echo "Crawling webpages......................"
-	python crawl.py data/US_web_search_list.Chrome data/google_bot_list 8
+	python crawl.py ../../data/US_web_search_list.Chrome ../../data/non_google_bot_list_1 8
+	previousdate=$nextdate
+	nextdate=$(date -d "$previousdate $gap" +%Y-%m-%d)
+	echo "previous"
+	echo $previousdate
+	echo "next"
+	echo $nextdate
+
+	# sleep until specific time
+	difference=$(($(date -d "$nextdate $googlehour" +%s) - $(date +%s)))
+	echo "Next run is on $nextdate $googlehour"
+	if [ $difference -lt 0 ]
+	then
+		echo "Sleeping $((86400 + difference)) seconds"
+		sleep $((86400 + difference))
+	else
+		echo "Sleeping $difference seconds"
+		sleep $difference
+	fi
+	echo "Crawling webpages......................"
+	python crawl.py ../../data/US_web_search_list.Chrome ../../data/google_bot_list 8
 	# update parameters
 	previousdate=$nextdate
 	nextdate=$(date -d "$previousdate $gap" +%Y-%m-%d)
