3eb27282c2908dc6e7f16a78884463cb8919b6af
Ruian Duan <duanruian@gmail.com>
2014-11-29 18:58:50 -0500

    Modify auto_crawl to zip the result after running.

--- src/utils/auto_crawl.sh
+++ src/utils/auto_crawl.sh
@@ -4,22 +4,25 @@ nongooglehour="16:00"
 gap="1 days"
 while true;
 do
-	nextdate=$(date +%Y-%m-%d)
+	nextdate=$(date +%Y%m%d)
 	# sleep until specific time
 	difference=$(($(date -d "$nextdate $nongooglehour" +%s) - $(date +%s)))
 	echo "Next run is on $nextdate $nongooglehour"
 	if [ $difference -lt 0 ]
 	then
 		echo "Sleeping $((86400 + difference)) seconds"
-		sleep $((86400 + difference))
+		# sleep $((86400 + difference))
 	else
 		echo "Sleeping $difference seconds"
 		sleep $difference
 	fi
 	echo "Crawling webpages......................"
 	python crawl.py ../../data/US_web_search_list.Chrome ../../data/non_google_bot_list_1 8
+	outdir=$(echo ../../data/US_web_search_list.Chrome*$nextdate*.crawl)
+	tgzfile=$outdir'.tgz'
+	tar -zcf $tgzfile $outdir
 
-	nextdate=$(date +%Y-%m-%d)
+	nextdate=$(date +%Y%m%d)
 	# sleep until specific time
 	difference=$(($(date -d "$nextdate $googlehour" +%s) - $(date +%s)))
 	echo "Next run is on $nextdate $googlehour"
@@ -33,4 +36,7 @@ do
 	fi
 	echo "Crawling webpages......................"
 	python crawl.py ../../data/US_web_search_list.Chrome ../../data/google_bot_list 8
+	outdir=$(echo ../../data/US_web_search_list.Chrome*$nextdate*.crawl)
+	tgzfile=$outdir".tgz"
+	tar -zcf $tgzfile $outdir
 done
