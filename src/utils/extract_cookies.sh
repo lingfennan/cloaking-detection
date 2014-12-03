e0da0932c92d796b2b08c6bf7a92bcfce276e64f
Ruian Duan <duanruian@gmail.com>
2014-12-03 15:42:30 -0500

    Add cookie reading scripts.

---
+++ src/utils/extract_cookies.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Usage:
+# extract_cookies.sh cookies.sqlite > cookies.txt
+# curl --cookie cookies.txt everlastinghelp.com | less
+
+
+function cleanup {
+rm -f $TMPFILE
+exit 1
+}
+
+trap cleanup  SIGHUP SIGINT SIGTERM
+
+# This is the format of the sqlite database:
+# CREATE TABLE moz_cookies (id INTEGER PRIMARY KEY, name TEXT, value TEXT, host TEXT, path TEXT,expiry INTEGER, lastAccessed INTEGER, isSecure INTEGER, isHttpOnly INTEGER);
+
+# We have to copy cookies.sqlite, because FireFox has a lock on it 
+TMPFILE=`mktemp /tmp/cookies.sqlite.XXXXXXXXXX`
+cat $1 >> $TMPFILE
+sqlite3 -separator '	' $TMPFILE << EOF
+.mode tabs
+.header off
+select host,
+case substr(host,1,1)='.' when 0 then 'FALSE' else 'TRUE' end,
+path,
+case isSecure when 0 then 'FALSE' else 'TRUE' end,
+expiry,
+name,
+value
+from moz_cookies;
+EOF
+cleanup
