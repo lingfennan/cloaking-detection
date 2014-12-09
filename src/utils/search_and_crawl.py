bf6891d0a8c4e0289676ae13d2a8cadb165972b2
Ruian Duan <duanruian@gmail.com>
2014-12-09 01:35:02 -0500

    Add notes for dagger. Add search term in proto.

--- src/utils/search_and_crawl.py
+++ src/utils/search_and_crawl.py
@@ -4,14 +4,24 @@
 #
 #
 # search 
+class Search:
+	def ad_links():
+		None
 
 # 
 # Iterate through all the popular words.
-# For the popular words.
+# For the word list.
+class PopularWord:
+	def words():
+		None
+
 
 # for each word, start a browser session to do google search on it, click and visit the landing page.
 #
 #
+class VisitSites:
+	def visit():
+		None
 
 
 
