300fcd441b740ba2e520b72361f5d78ec96469b2
Ruian Duan <duanruian@gmail.com>
2014-12-08 23:19:54 -0500

    Add search_and_crawl.py.

---
+++ src/utils/search_and_crawl.py
@@ -0,0 +1,17 @@
+# Usage:
+# python search_and_craw.py
+#
+#
+#
+# search 
+
+# 
+# Iterate through all the popular words.
+# For the popular words.
+
+# for each word, start a browser session to do google search on it, click and visit the landing page.
+#
+#
+
+
+
