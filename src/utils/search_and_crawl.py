e5e623b6380e4b0d1725df2cfbd740336484a61e
Ruian Duan <duanruian@gmail.com>
2015-01-11 00:48:26 -0500

    Complete basic function for search_and_crawl.py

--- src/utils/search_and_crawl.py
+++ src/utils/search_and_crawl.py
@@ -7,17 +7,27 @@
 #
 
 # from util import ad_list, hot_search_words
+import sys
+from datetime import datetime
 from selenium.webdriver.common.action_chains import ActionChains
-from crawl import UrlFetcher, set_browser_type
+from crawl import UrlFetcher, set_browser_type, hex_md5
 from learning_detection_util import valid_instance
 from thread_computer import ThreadComputer
 import proto.cloaking_detection_pb2 as CD
 
 class Search:
-	def ad_links(self, browser):
+	def __init__(self, crawl_config):
+		# user_agent should be set.
+		self.crawl_config = crawl_config
+		set_browser_type(self.crawl_config)
+		self.browser = start_browser(self.crawl_config.browser_type, incognito=False,
+				user_agent=self.crawl_config.user_agent)
+		self.browser.set_page_load_timeout(15)
+	
+	def ad_links(self):
 		clickstring_set = set()
 		try:
-			ads_ad_list = browser.find_elements_by_class_name('ads-ad')
+			ads_ad_list = self.browser.find_elements_by_class_name('ads-ad')
 			# print word
 			for ads_ad in ads_ad_list:
 				tags = ads_ad.find_elements_by_tag_name('a')
@@ -30,10 +40,10 @@ class Search:
 			None
 		return clickstring_set
 
-	def search_results(self, browser):
+	def search_results(self):
 		clickstring_set = set()
 		try:
-			search_list = browser.find_elements_by_class_name('g')
+			search_list = self.browser.find_elements_by_class_name('g')
 			for result_elem in search_list:
 				title_elem = result_elem.\
 						find_element_by_tag_name('h3')
@@ -50,12 +60,11 @@ class Search:
 			None	
 		return clickstring_set
 
-	def search(self, browser, search_term, search_config):
+	def search(self, search_term, search_config):
 		"""
 		Search search_term in browser. Return True if search succeeded.
 		@parmeter
 		search_term: the words to search
-		browser: browser handle controlled by selenium
 		result_type: either ad_links or search_results
 		top_count: the number of top search results to be inspected
 		@return
@@ -70,13 +79,13 @@ class Search:
 				url = 'https://www.google.com/?gws_rd=ssl#q='
 				url += '+'.join(search_term.split(' '))
 				url += '&start=' + start
-				browser.get(url)
+				self.browser.get(url)
 				if search_config.result_type == \
 						CD.SearchConfig.AD:
-					result_set = result_set | self.ad_links(browser)
+					result_set = result_set | self.ad_links()
 				elif search_ocnfig.result_type == \
 						CD.SearchConfig.SEARCH:
-					result_set = result_set | self.search_results(browser)
+					result_set = result_set | self.search_results()
 				else:
 					raise Exception("Unknown type of "
 							"SearchConfig.ResultType.")
@@ -91,9 +100,27 @@ class Search:
 # start = [8, 1, 0, 0, 0], end = [8, 2, , , 1]
 # means [US, past 7 days, all categories, sub categories, web search]
 class WordSet:
-	def collect():
+	def __init__(self, filename):
+		words = filter(bool, open(filename, 'r').read().split('\n'))
+		self.word_set = set()
+		for word in words:
+			self.word_set.add(word)
+	
+	def get_word_set(self):
+		return self.word_set
+	
+	def expand_word_set(self, expand_type):
+		if expand_type == "suggest":
+			self.google_suggest()	
+		elif expand_type == "search":
+			None
+		else:
+			None
+
+	def collect(self):
 		None
-	def google_suggest():
+
+	def google_suggest(self):
 		None
 
 # for each word, start a browser session to do google search on it,
@@ -104,6 +131,9 @@ class Visit:
 		# user_agent, user_agent_md5_dir should be set.
 		self.crawl_config = crawl_config
 		set_browser_type(self.crawl_config)
+		self.browser = start_browser(self.crawl_config.browser_type, incognito=False,
+				user_agent=self.crawl_config.user_agent)
+		self.browser.set_page_load_timeout(15)
 
 	def visit(clickstring_set):
 		# specify which type of browser to use
@@ -132,12 +162,29 @@ class Visit:
 		write_proto_to_file(crawl_log, self.crawl_log_filename)
 
 def main(argv):
+	# define constants 
 	user_UA = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/" \
 			"537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
 	google_UA = "AdsBot-Google (+http://www.google.com/adsbot.html)"
+	word_file = argv[0]
+	now = datetime.now().strftime("%Y%m%d-%H%M%S")
+	base_dir = word_file + '.' + now + '.selenium.crawl/'
+
 	crawl_config = CD.CrawlConfig()
 	crawl_config.maximum_threads = 6
 	crawl_config.user_agent = user_UA
+	crawl_config.user_agent_md5_dir = base_dir + hex_md5(crawl_config.user_agent) + '/'
+
 	print crawl_config.user_agent
 	print google_UA
+	search_config = CD.SearchConfig()
+	words = WordSet(word_file)
+	search = Search(crawl_config)
+	visit = Visit(crawl_config)
+	for word in words:
+		clickstring_set = search.search(word, search_config)
+		visit.visit(clickstring_set)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
 
