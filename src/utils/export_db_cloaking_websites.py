5c72fc633807e03f024b692da3201286a089d66f
Ruian Duan <duanruian@gmail.com>
2015-02-17 04:48:39 +0000

    Update the expected files.

--- src/utils/export_db_cloaking_websites.py
+++ src/utils/export_db_cloaking_websites.py
@@ -5,17 +5,20 @@ from learning_detection_util import write_proto_to_file, read_proto_from_file, v
 from thread_computer import ThreadComputer
 import proto.cloaking_detection_pb2 as CD
 
-
 def output_db(cursor, output_filename):
 	observed_sites = CD.ObservedSites()
-	for(url, userFilePath) in cursor:
+	url_site_map = dict()
+	for (url, userFilePath) in cursor:
+		if not url in url_site_map:
+			url_site_map[url] = CD.SiteObservations()
+			url_site_map[url].name = url
+		ob = url_site_map[url].observation.add()
+		ob.landing_url = url
+		ob.file_path = userFilePath
+	for url in url_site_map:
 		site = observed_sites.site.add();
-		site.name = url;
-		site_observation = site.observation.add();
-		site_observation.landing_url = url;
-		site_observation.file_path = userFilePath;
-	write_proto_to_file(observed_sites, output_filename);
-		
+		site.CopyFrom(url_site_map[url])
+	write_proto_to_file(observed_sites, output_filename)
 
 def export_db_to_file(table, outfile, labels=None):
 	cnx = MySQLdb.connect(host='localhost',user='root', passwd='asdf', db='LabelDB')
