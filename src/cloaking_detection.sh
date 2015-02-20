a458162c1009384d0c1bc58e1298649a1dd8055d
Ruian Duan <duanruian@gmail.com>
2015-02-20 17:30:10 -0500

    spammy ad detection result added

--- src/cloaking_detection.sh
+++ src/cloaking_detection.sh
@@ -6,21 +6,14 @@ GOOGLE_DOM_IN=$3
 # for search, use dom 10, c 3
 # for ad, use text r 10, c 2
 # for ad, use dom 8, c 2
-if [ "$TYPE" == "ad" ]; then
-	TEXT_R=8
-	TEXT_C=3
-	DOM_R=4
-	DOM_C=2
-else
-	TEXT_R=15
-	TEXT_C=3
-	DOM_R=8
-	DOM_C=2
-fi
+TEXT_R=15
+TEXT_C=2.1
+DOM_R=13
+DOM_C=1.8
 
 # The .learned suffix will be appended automatically
-python cluster_learning.py -f learn -i $GOOGLE_TEXT_IN -o $GOOGLE_TEXT_IN'.text' -t TEXT -c 1.1
-python cluster_learning.py -f learn -i $GOOGLE_DOM_IN -o $GOOGLE_DOM_IN'.dom' -t DOM -c 1.1
+python cluster_learning.py -f learn -i $GOOGLE_TEXT_IN -o $GOOGLE_TEXT_IN'.text' -t TEXT -c 0.7
+python cluster_learning.py -f learn -i $GOOGLE_DOM_IN -o $GOOGLE_DOM_IN'.dom' -t DOM -c 0.7
 python cloaking_detection.py -f detect -i $USER_IN'.text' -l $GOOGLE_TEXT_IN'.text.learned' -t TEXT -r $TEXT_R -c $TEXT_C
 python cloaking_detection.py -f detect -i $USER_IN'.dom' -l $GOOGLE_DOM_IN'.dom.learned' -t DOM -r $DOM_R -c $DOM_C
 RESULT=$USER_IN'.detection.result'
